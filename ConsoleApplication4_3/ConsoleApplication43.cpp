348725ecc457683d3b8c1ddf2592340affbb9d1e
ponychan <rzsckbmx@163.com>
2017-10-06 14:03:18 +0800

    4.3.cpp
    circle类的程序

--- ConsoleApplication4_3/ConsoleApplication43.cpp
+++ ConsoleApplication4_3/ConsoleApplication43.cpp
@@ -1,11 +1,47 @@
 // ConsoleApplication43.cpp: 定义控制台应用程序的入口点。
-//
+//4.3.cpp
 
 #include "stdafx.h"
+#include<iostream>
+using namespace std;
 
+const float pi = 3.141593;
+const float fprice = 35;
+const float cprice = 20;
+
+class circle {
+public:
+	circle(float r);
+	float circumference();
+	float area();
+private:
+	float radius;
+};
+
+circle::circle(float r) {
+	radius = r;
+}
+
+float circle::circumference() {
+	return 2 * pi*radius;
+}
+
+float circle::area() {
+	return pi*radius*radius;
+}
 
 int main()
 {
-    return 0;
+	float radius;
+	cout << "please enter radius:";
+	cin >> radius;
+	circle pool(radius);
+	circle poolrim(radius + 3);
+	float fencecost = poolrim.circumference()*fprice;
+	cout << "fenceing cost id $" << fencecost << endl;
+
+	float concretecost = (poolrim.area() - pool.area())*cprice;
+	cout << "concrete cost is $" << concretecost << endl;
+	return 0;
 }
 
